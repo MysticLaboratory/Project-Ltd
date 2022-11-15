036bf2ed0d54738810e1c6d3f0909d64898bbe89
MysticLaboratory <leonbalkissoon@gmail.com>
2022-11-14 22:44:31 -0400

    version1
    Co-Authored-By: Lando868 <115604383+lando868@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,12 @@
+import ToDoList from "./todolist.js";
+import ToDoItem from "./todoitem.js";
+
+
+const toDoList = new ToDoList();
+
+//Launch app
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        initApp();
+    }
+})
